1d34411ae37a7ca22e8047843f0c4c5ac4c78232
Jash Parekh <jparekh1@wayfair.com>
2021-02-02 20:15:09 -0500

    Revert setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,40 @@
-#!/usr/bin/env python
-
-# We still need a setup.py shim so we can still pip install -e .
-# https://snarky.ca/what-the-heck-is-pyproject-toml/#how-to-use-pyproject-toml-with-setuptools
+# type: ignore
+import ast
+import re
 
 import setuptools
 
-if __name__ == "__main__":
-    setuptools.setup()
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
+with open("gbq/__init__.py", "rb") as f:
+    _match = _version_re.search(f.read().decode("utf-8"))
+    if _match is None:
+        print("No version found")
+        raise SystemExit(1)
+    version = str(ast.literal_eval(_match.group(1)))
+
+
+setuptools.setup(
+    name="gbq",
+    version=version,
+    url="https://github.com/wayfair-incubator/gbq",
+    author="Jash Parekh",
+    author_email="jparekh1@wayfair.com",
+    description="Wrapper for Google BigQuery",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    package_data={"gbq": ["py.typed"]},
+    python_requires=">=3.6",
+    install_requires=["google-cloud-bigquery", "pydantic"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
