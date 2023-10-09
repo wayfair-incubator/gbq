86b441af2bed24b1a6243fc1975979a964294484
Jash Parekh <jp900u>
2023-10-09 13:12:47 +0530

    Do not upgrade pip

--- .github/actions/install-dependencies/action.sh
+++ .github/actions/install-dependencies/action.sh
@@ -1,8 +1,5 @@
 set -euo pipefail
 
-echo "Ensuring pip is up to date"
-python -m pip install --upgrade pip
-
 if [[ "${INSTALL_REQUIREMENTS}" == "true"  ]]; then
   echo "Installing code requirements"
   pip install -r requirements.lock
